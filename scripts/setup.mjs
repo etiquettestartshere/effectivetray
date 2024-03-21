824dcb02e33542ab5f701778da9eca34734b2ca3
alakshana <etiquettestartshere@gmail.com>
2024-03-21 07:28:43 -0400

    extends damageapplicationelement

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -1,12 +1,12 @@
 import { moduleSettings } from "./settings.mjs";
 import { effectiveSocket, effectiveTray, effectiveDamage } from "./effective-tray.mjs";
-import EffectiveDamageApplicationElement from "./damage-application.mjs";
+import EffectiveDAE from "./damage-application.mjs";
 
-window.customElements.define("effective-damage-application", EffectiveDamageApplicationElement);
+window.customElements.define("effective-damage-application", EffectiveDAE);
 
 Hooks.once("init", effectiveSocket.init);
 Hooks.once("init", moduleSettings.init);
 Hooks.once("init", effectiveTray.init);
 Hooks.once("init", effectiveDamage.init);
 
-export { EffectiveDamageApplicationElement };
+export { EffectiveDAE };
