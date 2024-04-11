8edb9ff1ccf35b0d4cf9352d29455238dea8f167
alakshana <etiquettestartshere@gmail.com>
2024-04-11 07:26:55 -0400

    fixed issue with tray appearing on whispered damage rolls

--- scripts/effective-tray.mjs
+++ scripts/effective-tray.mjs
@@ -238,6 +238,7 @@ export class effectiveDamage {
   static _damageTray(message, html) {
     if (message.flags?.dnd5e?.roll?.type === "damage") {
       if (!game.user.isGM) {
+        if (message.whisper.length && !message.whisper.includes(game.user.id)) return;
         const damageApplication = game.settings.get(MODULE, "damageTarget") ?
           document.createElement("effective-damage-application") :
           document.createElement("damage-application");
